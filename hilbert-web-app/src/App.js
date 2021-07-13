210d5be7da6288f9bef55992cd18712789c31518
Student <tarunvravi@gmail.com>
2021-07-13 15:59:48 -0400

    finished navbar ui

--- hilbert-web-app/src/App.js
+++ hilbert-web-app/src/App.js
@@ -1,25 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Box from "./Components/box/Box";
-
+import Navbar from "./Components/Navbar/Navbar"
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
-    fetch('/hilbert').then(res => res.json()).then(data => {
+    fetch('/linear').then(res => res.json()).then(data => {
       setCurrentTime(data.data);
     })
   }, [])
 
   return (
     <div className="App">
-      {
-        currentTime && currentTime.map(row => (
-          <div className="row">{row.map(element => <Box color={element} height={window.innerHeight/currentTime.length}/>)}</div>
+      <Navbar />
+      <div className="spaceFilling">
+        
+        {
+          currentTime && currentTime.map(row => (
+            <div className="row">{row.map(element => <Box color={element} height={(window.innerHeight-40)/currentTime.length}/>)}</div>
+            )
           )
-        )
-      }  
-      {currentTime &&  console.log(currentTime)}      
+        }  
+        {currentTime &&  console.log(currentTime)}    
+      </div>
     </div>
   );
 }
