d4cb10ab4a5006e3ddcf874d6300eedb51ed7598
Student <tarunvravi@gmail.com>
2021-07-14 15:35:11 -0400

    added light/dark theme + toggle

--- hilbert-web-app/src/App.js
+++ hilbert-web-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Box from "./Components/box/Box";
 import Navbar from "./Components/Navbar/Navbar";
+import { ThemeProvider } from "./Store/ThemeProvider";
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
@@ -14,23 +15,25 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="spaceFilling">
-        {currentTime &&
-          currentTime.map((row) => (
-            <div className="row">
-              {row.map((element) => (
-                <Box
-                  color={element}
-                  height={(window.innerHeight - 40) / currentTime.length}
-                />
-              ))}
-            </div>
-          ))}
-        {currentTime && console.log(currentTime)}
+    <ThemeProvider>
+      <div className="App">
+        <Navbar />
+        <div className="spaceFilling">
+          {currentTime &&
+            currentTime.map((row) => (
+              <div className="row">
+                {row.map((element) => (
+                  <Box
+                    color={element}
+                    height={(window.innerHeight - 40) / currentTime.length}
+                  />
+                ))}
+              </div>
+            ))}
+          {currentTime && console.log(currentTime)}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- hilbert-web-app/src/Components/Navbar/Navbar.js
+++ hilbert-web-app/src/Components/Navbar/Navbar.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./Navbar.css";
+import { ThemeContext } from "../../Store/ThemeProvider";
 
 function Navbar({ color, height }) {
+  const [theme, setTheme] = useContext(ThemeContext);
+
   return (
-    <nav className="Navbar">
-      <h1 className="titleText">
+    <nav className={"Navbar " + theme + "Bar"}>
+      <h1 className={"titleText " + theme + "TitleText"}>
         <span className="bold">Tarun Ravi's </span>Space Filling Curve
       </h1>
+
       <div className="buttons">
-        <h1 className="buttonText">Linear</h1>
-        <h1 className="buttonText">Hilbert</h1>
+        <h1
+          onClick={(theme) => setTheme("dark")}
+          className={"buttonText " + theme + "Text"}
+        >
+          Linear
+        </h1>
+
+        <h1
+          onClick={(theme) => setTheme("light")}
+          className={"buttonText " + theme + "Text"}
+        >
+          Hilbert
+        </h1>
+
+        <div
+          className="themeSwitch"
+          className={theme + "Switch" + " switch"}
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          <div className={theme + "Circle" + " switchCircle"}></div>
+        </div>
       </div>
     </nav>
   );
