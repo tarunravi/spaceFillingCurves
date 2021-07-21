c7b7b46f035878ab1e37da43b583e5ef0e9ceb57
Student <tarunvravi@gmail.com>
2021-07-21 10:01:03 -0400

    Added page redirect on upload

--- hilbert-web-app/src/Pages/UploadFile/UploadFile.js
+++ hilbert-web-app/src/Pages/UploadFile/UploadFile.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { useHistory } from "react-router-dom";
 function UploadFile() {
   const [file, setFile] = useState(null);
   const onChangeFile = (e) => {
     setFile(e.target.files[0]);
   };
-
+  const history = useHistory();
   const uploadFile = (e) => {
     e.preventDefault();
     const formData = new FormData();
@@ -17,6 +17,8 @@ function UploadFile() {
       .post("/api/upload", formData)
       .then((res) => console.log(res))
       .catch((err) => console.warn(err));
+
+    history.push("/Main");
   };
 
   return (
